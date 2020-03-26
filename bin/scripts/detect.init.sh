bda29b160d9fe1939d283e468513a559496b309e
Glenn L. Austin <glenn@austinsoft.com>
2020-03-26 08:46:55 -0700

    Detect launchd init type, put link to countly in /usr/local/bin since /usr/bin is for *distribution* binaries.

--- bin/scripts/detect.init.sh
+++ bin/scripts/detect.init.sh
@@ -11,12 +11,15 @@ then
 elif [[ `/sbin/init --version` =~ upstart ]];
 then
     INITSYS="upstart"
+elif [ -x /sbin/launchd -a -x /bin/launchctl ];
+then
+	INITSYS="launchd"
 fi 2> /dev/null
 
 bash $DIR/commands/$INITSYS/install.sh
 ln -sf $DIR/commands/$INITSYS/countly.sh $DIR/commands/enabled/countly.sh
 
 chmod +x $DIR/commands/countly.sh
-ln -sf $DIR/commands/countly.sh /usr/bin/countly
+ln -sf $DIR/commands/countly.sh /usr/local/bin/countly
 
 cp -f $DIR/commands/scripts/autocomplete/countly /etc/bash_completion.d
